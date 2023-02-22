499fda58de1f753950e2627f7ff1c65df3d2e607
Maksym Pokulytyi <mlightpro@gmail.com>
2023-02-22 16:00:14 +0200

    change the basename in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/ProxyBand/">
+        <BrowserRouter basename="/test_task_Routing-portal/">
             {/* <BrowserRouter> */}
             <App />
         </BrowserRouter>
