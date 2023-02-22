4cc4e551194e565c7dc988f4aa96a35ef92f48f4
Maksym Pokulytyi <mlightpro@gmail.com>
2023-02-22 16:10:28 +0200

    left BrowserRouter without basename to work locally

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/test_task_Routing-portal/">
-            {/* <BrowserRouter> */}
+        {/* <BrowserRouter basename="/test_task_Routing-portal/"> */}
+            <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
